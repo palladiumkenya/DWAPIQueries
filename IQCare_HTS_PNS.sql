351df5aa4d7cf216226b7d43056a807151ed8e3b
NgibuiniGikandi <ngibuinigikandi@gmail.com>
2019-12-17 11:17:01 +0300

    IQCare_PNS_Update

--- IQCare_HTS_PNS.sql
+++ IQCare_HTS_PNS.sql
@@ -1,5 +1,11 @@
 SELECT distinct pns.ptn_pk as PatientPK,a.SiteCode,a.FacilityName,
-'IQCare' as Emr, 'Kenya HMIS II' as Project,HTS_Number as HtsNumber, pd.PatientPK as PartnerPatientPk,pns.personid as PartnerPersonID,pns.AgeYears as Age, pns.PNSSex as Sex, RelationsipToIndexClient, ScreenedForIPV as ScreenedForIpv, IPVScreeningOutcome as IpvScreeningOutcome, CurrentlyLivingWithIndexClient, KnowledgeOfHIVStatus as KnowledgeOfHivStatus, PNSApproach as PnsApproach , PNSConsent as PnsConsent, Linked as LinkedToCare, b.dateEnrolled as LinkDateLinkedToCare,b.CCCNumber as CccNumber, b.FacilityEnrolled as FacilityLinkedTo, DATEADD(yy,-pns.Age,pns.Date) as Dob, pns.Date as DateElicited, pns.MaritalStatus
+'IQCare' as Emr, 'Kenya HMIS II' as Project,HTS_Number as HtsNumber, pd.PatientPK as PartnerPatientPk,pns.personid as PartnerPersonID,
+pns.AgeYears as Age, pns.PNSSex as Sex, RelationsipToIndexClient, ScreenedForIPV as ScreenedForIpv, 
+IPVScreeningOutcome as IpvScreeningOutcome, CurrentlyLivingWithIndexClient, KnowledgeOfHIVStatus as KnowledgeOfHivStatus, 
+PNSApproach as PnsApproach , PNSConsent as PnsConsent, Linked as LinkedToCare, b.dateEnrolled as LinkDateLinkedToCare,
+b.CCCNumber as CccNumber, b.FacilityEnrolled as FacilityLinkedTo, pd.dateofbirth as Dob, 
+pns.Date as DateElicited, 
+pns.MaritalStatus
   FROM  tmp_PNS_Register pns 
     INNER JOIN dbo.tmp_HTS_LAB_register AS c ON pns.Ptn_pk = c.PatientPK
 	INNER JOIN tmp_PatientMaster AS a ON pns.Ptn_pk = a.PatientPK
